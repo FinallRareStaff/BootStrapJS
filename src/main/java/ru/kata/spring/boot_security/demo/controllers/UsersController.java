47a184c106e4f47bc61d92671e33f2dfb0d8347a
FinallRareStaff <Jeymester@gmail.com>
2022-05-17 12:19:07 +0300

    rework UsersController.java

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/UsersController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/UsersController.java
@@ -1,6 +1,8 @@
 package ru.kata.spring.boot_security.demo.controllers;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -16,38 +18,27 @@ import java.util.Set;
 public class UsersController {
 
     private final UserService userService;
-    private final RoleService roleService;
 
-    public UsersController(UserService userService, RoleService roleService) {
+    public UsersController(UserService userService) {
         this.userService = userService;
-        this.roleService = roleService;
     }
 
     @GetMapping("/user")
     public String user(Model model, Principal principal) {
         if (principal != null) {
-            User user = userService.loadUserByUsername(principal.getName());
+        User user = userService.loadUserByUsername(principal.getName());
             Set<String> roles = AuthorityUtils.authorityListToSet(user.getAuthorities());
-            boolean roleAdmin = roles.contains("ROLE_ADMIN");
-            model.addAttribute("username", principal.getName());
-            model.addAttribute("user", user);
-            model.addAttribute("roleAdmin", roleAdmin);
+            model.addAttribute("roleAdmin", roles.contains("ROLE_ADMIN"));
         }
-        model.addAttribute("users", userService.getAllUsers());
         return "users/user";
     }
 
     @GetMapping("/admin")
     public String admin(Model model, Principal principal) {
         if (principal != null) {
-            String stringRoleList = new String();
-            User user = userService.loadUserByUsername(principal.getName());
-            model.addAttribute("username", principal.getName());
-            model.addAttribute("user", user);
-            model.addAttribute("rolesAll", roleService.getAllRoles());
+            String stringRoleList = "";
             model.addAttribute("stringRoleList", stringRoleList);
         }
-        model.addAttribute("users", userService.getAllUsers());
         return "users/admin";
     }
 
